f08bb666bde7e6ba234ce4e24bb54a21c1027219
Adrian Rodriguez <adrorodri@gmail.com>
2021-10-13 09:15:45 -0400

    Error Dialog

--- app/src/main/java/edu/upb/cocinaya/ui/login/fragments/loginErrror/LoginErrorFragment.kt
+++ app/src/main/java/edu/upb/cocinaya/ui/login/fragments/loginErrror/LoginErrorFragment.kt
@@ -5,9 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import edu.upb.cocinaya.R
 
-class LoginErrorFragment : Fragment() {
+class LoginErrorFragment : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
